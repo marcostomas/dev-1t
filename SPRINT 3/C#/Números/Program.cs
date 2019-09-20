39a8ec0a7e7f917d0896e48cec5f7b33df34bbe3
Marcos Tomás <mtomasferreira@hotmail.com>
2019-09-20 10:29:07 -0300

    Exercícios  de Sexta

---
+++ SPRINT 3/C#/Números/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Números
+{
+     class Program
+    {
+        static void Main(string[] args)
+        { 
+
+            int numero = 0;
+
+            do{
+                Console.WriteLine("=========================");
+                Console.WriteLine("Este Número É Par ou Ímpar?");
+                Console.WriteLine("=========================");
+
+                Console.WriteLine("Informe um Número");
+                numero = int.Parse(Console.ReadLine());
+
+
+                if(numero != 0){
+
+                    if(numero %2 == 0) {
+                        Console.WriteLine("Este Número É Par!");
+                    }
+
+                    else{
+                        Console.WriteLine("Este Número É Ímpar!");
+                    }
+                
+                    Console.WriteLine("Caso Queira Sair do Programa, Aperte (0)");
+                }
+
+            }while(numero != 0);
+
+        }
+
+    }
+}
