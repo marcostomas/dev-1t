68abbd1c1cbd1335e185d96f4736206351f20eff
Marcos Tomás <mtomasferreira@hotmail.com>
2019-09-23 07:38:36 -0300

    exc finais sexta

---
+++ SPRINT 3/C#/NoÍnpar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace nuimpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+           for(int i = 1; i <= 99; i+=2)
+           {
+
+            Console.WriteLine(i);
+           }
+
+        }
+    }
+}
---
+++ SPRINT 3/C#/Tabuada/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int c = 0; c<=50; c+=5)
+            Console.Write(c + "-");
+        }
+    }
+}
