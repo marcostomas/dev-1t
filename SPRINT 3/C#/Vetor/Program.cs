85c6a61ec1edb59a406ffaaa420eb5428c8afd39
Marcos Tomás <mtomasferreira@hotmail.com>
2019-09-26 11:19:27 -0300

    Exercícios 26/09

--- SPRINT 3/C#/Vetor/Program.cs
+++ SPRINT 3/C#/Vetor/Program.cs
@@ -3,9 +3,28 @@
 namespace Vetor {
     class Program {
         static void Main (string[] args) {
-            
-            int[] vetor = new int [5];
-            Console.WriteLine(vetor);
+
+            int[] vetor = new int [6];
+
+            for(int cont=0; cont < 6; cont++){
+                Console.WriteLine("Digite um número");
+                vetor [cont] = int.Parse(Console.ReadLine());
+            }
+
+            foreach(int num in vetor){
+                
+                if(num%2 == 0){
+                    pares += 1;
+                    
+                    // Formas Diferentes de se fazer
+                    // pares = pares + 1;
+                    // pares++ (só funciona para MAIS 1)
+                }
+
+                else{
+                    impares++;
+                }
+            }
         }
     }
 }
---
+++ SPRINT 3/C#/tmedia/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace tmedia {
+    class Program {
+        static void Main (string[] args) {
+            double maior = 0;
+            double menor = 0;
+
+            double[] temperaturas = new double[12];
+
+            for (int i = 0; i < 12; i++) {
+                Console.WriteLine ($"Digite a temperatura do mês {i+1}:");
+                temperaturas[i] = double.Parse (Console.ReadLine ());
+            }
+
+            maior = temperaturas[0];
+            menor = temperaturas[0];
+
+            foreach (double temp in temperaturas) {
+
+                if (temp > maior) {
+                    maior = temp;
+                } else if (temp < menor) {
+                    menor = temp;
+                }
+            }
+
+            Console.WriteLine ($"A maior Temperatura deste ano foi: {maior}°C");
+
+            Console.WriteLine ($"A menor Temperatura deste ano foi: {menor}°C");
+        }
+    }
+}
