71e509d260a9abb29898731e51a84cebda0bf142
Marcos Tomás <mtomasferreira@hotmail.com>
2019-09-27 11:17:00 -0300

    Excercício POO 27/09

--- SPRINT 3/C#/ConvAge/Program.cs
+++ SPRINT 3/C#/ConvAge/Program.cs
@@ -1,24 +1,43 @@
 ﻿using System;
 
-namespace ConvAge
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ConvAge {
+    class Program {
+        static void Main (string[] args) {
             
-            byte age  = 0;
-            
-            
-            Console.WriteLine("==========================");
-            Console.WriteLine("Insira Sua Idade");
-            Console.WriteLine("==========================");
-            
-            
-            Console.WriteLine("Digite Sua Idade");
-            age = byte.Parse(Console.ReadLine());
+            int num1 = 0;
+            int num2 = 0;
+            string oper;
 
-            age = 
+            Console.WriteLine ("Console Calculadora de idade em C#\r");
+            Console.WriteLine ("========================\n");
+
+            Console.WriteLine ("Insira sua idade");
+            num1 = int.Parse (Console.ReadLine ());
+
+            Console.WriteLine ("Quer converter para qual?");
+            Console.WriteLine ("\ta - Meses");
+            Console.WriteLine ("\ts - Dias");
+            Console.WriteLine ("\tm - Horas");
+            Console.WriteLine ("\td - Minutos");
+
+            switch (Console.ReadLine ()) {
+                case "a":
+                    Console.WriteLine ($"Resultado: {num1} * {12} = " + (num1 * 12));
+                    break;
+                case "s":
+                    Console.WriteLine ($"Resultado: {num1} * {365} = " + (num1 * 365));
+                    break;
+                case "m":
+                    Console.WriteLine ($"Resultado: {num1} * {8640} = " + (num1 * 8640));
+                    break;
+                case "d":
+                    Console.WriteLine ($"Resultado: {num1} * {518400} = " + (num1 * 518400));
+                    break;
+
+            }
+
+            Console.Write ("Aperte qualquer tecla para encerrar o programa");
+            Console.ReadKey ();
         }
     }
 }
---
+++ SPRINT 3/C#/McBonalds/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace McBonalds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente1 = new Cliente ("Marcos", "+55 11 91234-5678", "marcos@email.com");
+
+            Console.WriteLine("Nome: " + cliente1.Nome);
+            Console.WriteLine("Telefone: " + cliente1.Telefone);
+            Console.WriteLine("Email: " + cliente1.Telefone);
+        } 
+    }
+}
