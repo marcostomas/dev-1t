b61eba1adc6312c8c36ae378d2b5f0b12babd83f
Marcos Tomás <mtomasferreira@hotmail.com>
2019-10-01 07:52:05 -0300

    Fix ConvAge

--- SPRINT 3/C#/ConvAge/Program.cs
+++ SPRINT 3/C#/ConvAge/Program.cs
@@ -5,8 +5,8 @@ namespace ConvAge {
         static void Main (string[] args) {
             
             int num1 = 0;
-            int num2 = 0;
-            string oper;
+            // int num2 = 0;
+            // string oper;
 
             Console.WriteLine ("Console Calculadora de idade em C#\r");
             Console.WriteLine ("========================\n");
@@ -15,28 +15,28 @@ namespace ConvAge {
             num1 = int.Parse (Console.ReadLine ());
 
             Console.WriteLine ("Quer converter para qual?");
-            Console.WriteLine ("\ta - Meses");
-            Console.WriteLine ("\ts - Dias");
-            Console.WriteLine ("\tm - Horas");
-            Console.WriteLine ("\td - Minutos");
+            Console.WriteLine ("\tm - Meses");
+            Console.WriteLine ("\td - Dias");
+            Console.WriteLine ("\th - Horas");
+            Console.WriteLine ("\tn - Minutos");
 
             switch (Console.ReadLine ()) {
-                case "a":
+                case "m":
                     Console.WriteLine ($"Resultado: {num1} * {12} = " + (num1 * 12));
                     break;
-                case "s":
+                case "d":
                     Console.WriteLine ($"Resultado: {num1} * {365} = " + (num1 * 365));
                     break;
-                case "m":
+                case "h":
                     Console.WriteLine ($"Resultado: {num1} * {8640} = " + (num1 * 8640));
                     break;
-                case "d":
+                case "n":
                     Console.WriteLine ($"Resultado: {num1} * {518400} = " + (num1 * 518400));
                     break;
 
             }
 
-            Console.Write ("Aperte qualquer tecla para encerrar o programa");
+            Console.WriteLine("Aperte qualquer tecla para encerrar o programa");
             Console.ReadKey ();
         }
     }
